4f96e13da8bd592f4357085db0318cdc729fb5af
Tanu Singh <70361516+MERN-Tanu@users.noreply.github.com>
2023-11-08 11:46:59 +0530

    Update index.js

--- Frontend-by-Sanket/Javascript/Coercion-type-conversion/index.js
+++ Frontend-by-Sanket/Javascript/Coercion-type-conversion/index.js
@@ -1,6 +1,6 @@
 # Addition
   
-``` 
+
 console.log(10 + null);
 console.log(10 + undefined);
 console.log(10 + NaN);
@@ -9,9 +9,8 @@ console.log(10 + false);
 console.log(10 + "tanu");
 console.log(10 - $);
 console.log(10 - 10);
-```
 # Subtraction
-```
+
 console.log(10 - null);
 console.log(10 - undefined);
 console.log(10 - NaN);
@@ -24,13 +23,15 @@ console.log(1 - "0*a");
 console.log("s" - "s");
 console.log("1" - "1");
 
+```
 var x = 10 + "10";
 console.log(x);
 var y = "20" + 10;
 console.log(y);
 var z = true + "true";
 console.log(z);
-
+```
+```
 var w = 10 - "5";
 console.log(w);
 var x = 10 * 5;
