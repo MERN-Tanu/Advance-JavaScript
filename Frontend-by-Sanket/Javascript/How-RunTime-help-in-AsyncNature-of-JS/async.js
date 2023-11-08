a0d3a2d2da74de29dee7971a92470e195ac88ee7
Tanu Singh <70361516+MERN-Tanu@users.noreply.github.com>
2023-11-08 11:49:20 +0530

    Update async.js

--- Frontend-by-Sanket/Javascript/How-RunTime-help-in-AsyncNature-of-JS/async.js
+++ Frontend-by-Sanket/Javascript/How-RunTime-help-in-AsyncNature-of-JS/async.js
@@ -1,10 +1,9 @@
-/**console.log("Hi we are starting");
+```console.log("Hi we are starting");
 for (let i = 0; i < 1000000000; i++) {
   // some task
 }
 
 console.log("done");
-*/
 
 // console.log("hi");
 // setTimeout(function () {
@@ -12,23 +11,22 @@ console.log("done");
 // }, 5000);
 // console.log("by");
 
-/***
- * output:  hi
-            by
-            time done
- *
- */
+output:  hi
+         by
+         time done
+```
 
-// // --------------
-// function timeConsumingByLoop() {
-//   console.log("loop starts");
-//   for (let i = 0; i < 10000000; i++) {
-//     // some task
-//   }
-//   console.log("loop ends");
-// }
+```
+function timeConsumingByLoop() {
+  console.log("loop starts");
+   for (let i = 0; i < 10000000; i++) {
+    // some task
+  }
+ console.log("loop ends");
+ }
+```
 
-// function timeConsumingByRunTime() {
+``` function timeConsumingByRunTime() {
 //   console.log("starting timer");
 //   setTimeout(function exec() {
 //     console.log("completed the timer");
@@ -39,20 +37,23 @@ console.log("done");
 // timeConsumingByRunTime();
 
 // console.log("by");
-// // --------------------------------------------------------------
+```
 
+```
 // console.log("hello world");
 
 // // In the setTimeout it won't block the code for 0 second.It is only block in for loop onlhy
 // setTimeout(function exec() {
 //   console.log("timer done");
 // }, 10);
-
+```
+```
 // for (let i = 0; i < 100000; i++) {
 //   // some task //10sec
 // }
 // console.log("end");
-
+```
+```
 console.log("Hello world");
 setTimeout(function exec() {
   console.log("timer done");
@@ -64,4 +65,5 @@ setTimeout(function exec() {
 for (var i = 0; i < 1000; i++) {
   // some task 10s
 }
+```
 console.log("end");
